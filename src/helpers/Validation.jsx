60c53deec56037ec67ec312dc56931978b5b00f4
AhWoDestroyer <kostailusenko911@gmail.com>
2023-03-31 16:22:50 +0300

    pull-req-correction-03

--- src/helpers/Validation.jsx
+++ src/helpers/Validation.jsx
@@ -1,11 +1,10 @@
 export default async function Validate({ password, email, name }) {
 	if (password && email && name) {
-			await post('/register', {
-				password: password,
-				email: email,
-				name: name,
-			})
-		)
+		(await post('/register', {
+			password: password,
+			email: email,
+			name: name,
+		}))
 			? alert(`navigate('/login')`)
 			: alert('err');
 	}
